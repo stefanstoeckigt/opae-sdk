c5e94d48ba58c247df2f6b81ae53845570e02ffe
Rodrigo Rojo <rodrigo.rojo@intel.com>
2018-01-20 01:26:29 +0000

    CI: add branch name to git push command

--- scripts/merge-master.sh
+++ scripts/merge-master.sh
@@ -3,7 +3,7 @@
 git merge --ff-only origin/master
 if [ $? == 0 ]; then
     echo "Successfully merged master"
-    git push https://$GITHUB_SECRET_TOKEN@github.com/OPAE/opae-sdk.git > /dev/null 2>&1
+    git push https://$GITHUB_SECRET_TOKEN@github.com/OPAE/opae-sdk.git coverity_scan > /dev/null 2>&1
 else
     echo "Problem merging master"
     exit -1
