7ac9272ef7f2ec76aad44b4119e1139d52e10c82
Rodrigo Rojo <rodrigo.rojo@intel.com>
2018-01-19 18:13:53 -0800

    CI: change branch name merge-master.sh
    Change origin/master to master

--- scripts/merge-master.sh
+++ scripts/merge-master.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 git fetch origin master
-git merge --ff-only origin/master
+git merge --ff-only master
 if [ $? == 0 ]; then
     echo "Successfully merged master"
     git push https://$GITHUB_SECRET_TOKEN@github.com/OPAE/opae-sdk.git coverity_scan > /dev/null 2>&1
